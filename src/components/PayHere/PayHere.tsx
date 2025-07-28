6bcb2caadcab95bf866b223b74ee938d56f83b25
SamanPrasad <samanseedr2@gmail.com>
2025-07-28 17:50:38 +0530

    logos

--- src/components/PayHere/PayHere.tsx
+++ src/components/PayHere/PayHere.tsx
@@ -23,38 +23,45 @@ function PayHere() {
     <div>
       <h1>PayHere</h1>
       <form action="https://sandbox.payhere.lk/pay/checkout" method="post">
-        <input type="text" name="merchant_id" value="1231349" />
+        <input type="hidden" name="merchant_id" value="1231349" />
         <input
-          type="text"
+          type="hidden"
           name="return_url"
-          value="https://payhereclient.vercel.app/"
+          value="https://payhereclient.vercel.app"
         />
         <input
-          type="text"
+          type="hidden"
           name="cancel_url"
-          value="https://payhereclient.vercel.app/"
+          value="https://payhereclient.vercel.app"
         />
         <input
-          type="text"
+          type="hidden"
           name="notify_url"
           value="https://testnext-five-sigma.vercel.app/api/payhere/webhook"
         />
         <h4>Item Details</h4>
-        <input type="text" name="order_id" defaultValue="12345" />
-        <input type="text" name="items" defaultValue="Door bell wireless" />
-        <input type="text" name="currency" defaultValue="LKR" />
-        <input type="text" name="amount" defaultValue="1000" />
+        <input type="hidden" name="order_id" defaultValue="12345" />
+        <input type="hidden" name="items" defaultValue="Door bell wireless" />
+        <input type="hidden" name="currency" defaultValue="LKR" />
+        <input type="hidden" name="amount" defaultValue="1000" />
         Customer Details
-        <input type="text" name="first_name" defaultValue="Saman" />
-        <input type="text" name="last_name" defaultValue="Perera" />
-        <input type="text" name="email" defaultValue="samanp@gmail.com" />
-        <input type="text" name="phone" defaultValue="0771234567" />
-        <input type="text" name="address" defaultValue="No.1, Galle Road" />
-        <input type="text" name="city" defaultValue="Colombo" />
+        <input type="hidden" name="first_name" defaultValue="Saman" />
+        <input type="hidden" name="last_name" defaultValue="Perera" />
+        <input type="hidden" name="email" defaultValue="samanp@gmail.com" />
+        <input type="hidden" name="phone" defaultValue="0771234567" />
+        <input type="hidden" name="address" defaultValue="No.1, Galle Road" />
+        <input type="hidden" name="city" defaultValue="Colombo" />
         <input type="hidden" name="country" value="Sri Lanka" />
         <input type="hidden" name="hash" value={hash} />
         <button>Buy Now</button>
       </form>
+      <a href="https://www.payhere.lk" target="_blank">
+        <img
+          src="https://www.payhere.lk/downloads/images/payhere_long_banner_dark.png"
+          alt="PayHere"
+          width="400"
+        />
+      </a>
     </div>
   );
 }
