3dd063ed96d5bb88eadbf6535ca44dec9ddab72f
SamanPrasad <samanseedr2@gmail.com>
2025-07-30 03:08:24 +0530

    added sbscripton

--- src/components/Stripe/Subscription/Subscription.tsx
+++ src/components/Stripe/Subscription/Subscription.tsx
@@ -1,11 +1,16 @@
 import axios from "axios";
 
 function Subscription() {
-  const handleSubscription = async () => {
-    const result = await axios.post("/stripe/subscription/checkout");
-
-    const url = result.data;
-    window.location.href = url;
+  const handleSubscription = () => {
+    axios
+      .post("/stripe/subscription/checkout")
+      .then((res) => {
+        const url = res.data;
+        window.location.href = url;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
