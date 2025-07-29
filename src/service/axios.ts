57f88712f8d4615817000c0e33b61b0a16f2122b
SamanPrasad <samanseedr2@gmail.com>
2025-07-30 01:35:59 +0530

    baseurl

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const client = axios.create({
-  baseURL: "https://testnext-five-sigma.vercel.app/api",
-  // baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.BASE_URL,
 });
